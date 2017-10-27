2b9156ee7d873a476ecc31fb5cee078edf353ff4
Malemute <erlinn@fromru.com>
2017-10-27 15:54:41 +0300

    First draft for PrettyPrint module

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
+import sys
 import json
 
 
 def load_data(filepath):
-    pass
+	with open(filepath, 'r') as string_from_file:
+		return json.load(string_from_file)
 
 
 def pretty_print_json(data):
-    pass
+	return json.dumps(data, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
-    pass
+	filepath = sys.argv[0]
+	parsed_json = load_data(filepath)
+	print pretty_print_json(parsed_json)
