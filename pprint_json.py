733b0dc962d22c2e39615fb6e405b66808868a01
Malemute <erlinn@fromru.com>
2017-10-28 10:53:50 +0300

    UTF fixed, test passed

--- pprint_json.py
+++ pprint_json.py
@@ -3,16 +3,16 @@ import json
 
 
 def load_data(filepath):
-	with open(filepath, 'r') as string_from_file:
-		return json.load(string_from_file)
+    with open(filepath, 'r') as the_file:
+        return json.load(the_file)
 
 
 def pretty_print_json(data):
-	return json.dumps(data, indent=4, sort_keys=True)
+    return json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-	filepath = sys.argv[0]
-	parsed_json = load_data(filepath)
-	pretty_printed_json = pretty_print_json(parsed_json)
-	print(pretty_printed_json)
+    filepath = sys.argv[1]
+    parsed_json = load_data(filepath)
+    pretty_printed_json = pretty_print_json(parsed_json)
+    print(pretty_printed_json)
